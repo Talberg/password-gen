34560445cb5f7f3e04a2ff01a21fc1449df66250
Alexander Talberg <alexandermtalberg@gmail.com>
2019-12-12 22:08:15 -0700

    Made alot of changes... Got the buttons wired up, wrote code to prevent a null in all the confirm statments. made the copy to clipboard function using the browsers built in code.

--- java.js
+++ java.js
@@ -4,11 +4,58 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
-var useUpper = confirm('Would you like to use uppercase in your password?')
-var useLower = confirm('Would you like to use lowercase in your password?')
-var useSpecial = confirm('Would you like to use speucial in your password?')
-var useNum = confirm('Would you like to use numbers in your password?')
-var prompt = prompt('Please select desired the length of your password, number 8-128')
+var textArea = document.getElementById('password')
+var ranPassword = false
+var button1El= document.querySelectorAll('#generate')
+var footerEl = document.querySelector('.card-footer')
+
+
+footerEl.setAttribute('style','background-color:white')
+console.log(button1El)
+
+// button1El.setAttribute('style', 'border-radius: 5px; background-color:red;)
+
+button1El[0].setAttribute('style', 'border-radius:20px; background-color:firebrick; color:white;  ')
+button1El[1].setAttribute('style', 'border-radius:20px; background-color:lightgray; color:white;  ')
+textArea.setAttribute('style','border:dashed; border-color:gray; text-align:center; margin-left:20% ;margin-right:20%')
+// var useUpper = confirm('Would you like to use uppercase in your password?')
+// var useLower = confirm('Would you like to use lowercase in your password?')
+// var useSpecial = confirm('Would you like to use speucial in your password?')
+// var useNum = confirm('Would you like to use numbers in your password?')
+// var prompt = prompt('Please select desired the length of your password, number 8-128')
+
+
+// function generatePass(){
+//  var useUpper = confirm('Would you like to use uppercase in your password?')
+// var useLower = confirm('Would you like to use lowercase in your password?')
+// var useSpecial = confirm('Would you like to use special in your password?')
+// var useNum = confirm('Would you like to use numbers in your password?')
+// var prompt = prompt('Please select desired the length of your password, number 8-128')
+// addToUsable(useUpper, upperCase)
+// addToUsable(useLower, lowerCase)
+// addToUsable(useSpecial, specialChar)
+// addToUsable(useNum,num)
+// alert(password.join(""))
+// }
+
+
+function copyButton(){
+    var copyText = textArea;
+    copyText.select();
+    document.execCommand('copy');
+    copyText.setSelectionRange(0, 99999)
+    alert(copyText)
+}
+
+
+
+
+
+
+
+// pushes the arrays if the user confirms the question using the T/F 
+
+
 
 
 
@@ -23,25 +70,78 @@ function addToUsable(confirm, arr) {
     }
 }
 
-addToUsable(useUpper, upperCase)
-addToUsable(useLower, lowerCase)
-addToUsable(useSpecial, specialChar)
-addToUsable(useNum,num)
+// addToUsable(useUpper, upperCase)
+// addToUsable(useLower, lowerCase)
+// addToUsable(useSpecial, specialChar)
+// addToUsable(useNum,num)
 
-for( i = 0; i < prompt; i++){
+
+
+
+console.log(Array.toString(password))
+
+textArea.textContent = usableChar
+
+
+
+function randomize (){
+ var numChar = prompt('Please select desired the length of your password, number 8-128')
+
+
+
+  for( i = 0; i < numChar; i++){
     password.push(usableChar[Math.floor(Math.random() * usableChar.length)])
+
+   
+}
+
+ranPassword = true
+
+
+if(ranPassword){
+
+    textArea.textContent = (password.join(''))
 }
 
 
-alert(password)
+
+   }
 
 
+function generatePass(){
+    
+   var useUpper = confirm('Would you like to use uppercase in your password?')
+   var useLower = confirm('Would you like to use lowercase in your password?')
+   var useSpecial = confirm('Would you like to use special in your password?')
+   var useNum = confirm('Would you like to use numbers in your password?')
+ 
+    addToUsable(useUpper, upperCase)
+    addToUsable(useLower, lowerCase)
+    addToUsable(useSpecial, specialChar)
+    addToUsable(useNum,num)
+ 
+ 
+ 
+   if(useLower === false && useSpecial === false && useSpecial === false && useNum === false ){
+       alert('Please choose at least 1.')
+       generatePass()
+   }
 
+   
+  
 
+   console.log(password)
+    
+   randomize()
 
+   }
+   
+   
 
-console.log(usableChar)
-
-
-
-
+   function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+  }
